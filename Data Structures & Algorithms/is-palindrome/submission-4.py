a8d80f6432b2dc32ccfc6aca261df1ee17aff152
Khai <1192669+khaister@users.noreply.github.com>
2026-07-15 16:28:21 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            # ignore non-alphanumeric values
+            if not s[i].isalnum():
+                i += 1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
+
+            # if at least one pair not matching, 
+            # then not a valid palindrome,
+            # so no need to check the rest
+            if s[i].lower() != s[j].lower():
+                return False
+
+            i += 1
+            j -= 1
+
+        # all letter pairs match, so must be a palindrome
+        return True
