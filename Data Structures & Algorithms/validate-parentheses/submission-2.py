48e899e3bcb1d0ff7d784c376f357135111993da
Khai <1192669+khaister@users.noreply.github.com>
2026-07-21 19:35:18 -0700

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = []
+        for current_bracket in s:
+            if current_bracket in ["(", "{", "["]:
+                brackets.append(current_bracket)
+            else:
+                previous_bracket = brackets[-1] if brackets else None
+
+                # not matching, so entire string s in invalid, returns early
+                if (
+                    (current_bracket == ")" and previous_bracket != "(")
+                    or (current_bracket == "}" and previous_bracket != "{")
+                    or (current_bracket == "]" and previous_bracket != "[")
+                ):
+                    return False
+
+                # there's a matching closing bracket, so removed it
+                brackets.pop()
+
+        # if all matched, then there should not be anything in brackets
+        return len(brackets) == 0
